c18583cf7ddf2ebabadafd895ef44241e085280b
mr4040 <67740031+mr4040@users.noreply.github.com>
2026-01-09 02:57:15 +0100

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["'JetBrains Mono'", "monospace"],
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+};
+
+export default config;
